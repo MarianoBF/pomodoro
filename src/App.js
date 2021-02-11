c31e26ab1de170a8116c2581cf14d7671a2f243b
MarianoBF <mariano.b.fernandez@gmail.com>
2021-02-11 18:44:40 -0300

    Inicial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
-  return (
+  const [tiempo, setTiempo] = useState(0)
+  useEffect(() => {
+    const iniciar = setInterval(() => {setTiempo(tiempo => tiempo + 1)},1000);
+    
+    return () => {clearInterval(iniciar)}
+
+  })
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <p id="break-label">Tiempo de pausa</p>
+    <p id="break-label">Largo de sesión</p>
+
+    <button id="break-decrement">Menos pausa</button>
+    <button id="break-increment">Más pausa</button>
+    
+    <button id="session-decrement">Menos sesión</button>
+    <button id="session-increment">Más sesión</button>
+
+    <input type="text" id="break-length" value="5" readOnly />
+
+    <input type="text" id="session-length" value="25" readOnly />
+
+    <p id="timer-label">{/*condicional segun estado Session or not*/}</p>
+
+    <p id="time-left">{tiempo > 60 ? Math.floor(tiempo / 60) : "00"}:{tiempo % 60}</p>
+
+    <button id="start_stop" onClick={null}>{/*Quizás ID guión medio?*/}Iniciar/Parar</button>
+    <button id="start_stop" onClick={null}>{/*Quizás ID guión medio?*/}Iniciar/Parar</button>
+
+    <button id="reset">Resetear</button>
+
     </div>
   );
 }
